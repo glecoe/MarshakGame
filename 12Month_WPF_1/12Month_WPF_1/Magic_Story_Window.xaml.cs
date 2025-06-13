49f4a3534558fd0c5f35f02693a908f9bfd5dce5
IsakovIlyaUniversity <isakov.ilya.university@gmail.com>
2025-06-13 19:11:51 +0300

    #4_Main_Menu_1

---
+++ 12Month_WPF_1/12Month_WPF_1/Magic_Story_Window.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _12Month_WPF_1
+{
+    /// <summary>
+    /// Interaction logic for Magic_Story_Window.xaml
+    /// </summary>
+    public partial class Magic_Story_Window : Window
+    {
+        public static Magic_Story_Window _Magic_Story;
+        public Magic_Story_Window()
+        {
+            InitializeComponent();
+            _Magic_Story = this;
+            this.WindowState = WindowState.Maximized;
+            this.WindowStyle = WindowStyle.None;
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow Main_Window = new MainWindow();
+            this.Close();
+            Main_Window.Show();
+        }
+    }
+}
---
+++ 12Month_WPF_1/12Month_WPF_1/Settings_Window.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _12Month_WPF_1
+{
+    /// <summary>
+    /// Interaction logic for Settings_Window.xaml
+    /// </summary>
+    public partial class Settings_Window : Window
+    {
+        public static Settings_Window _Settings_Window;
+        public Settings_Window()
+        {
+            InitializeComponent();
+            _Settings_Window = this;
+            this.WindowState = WindowState.Maximized;
+            this.WindowStyle = WindowStyle.None;
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow Main_Window = new MainWindow();
+            this.Close();
+            Main_Window.Show();
+        }
+    }
+}
