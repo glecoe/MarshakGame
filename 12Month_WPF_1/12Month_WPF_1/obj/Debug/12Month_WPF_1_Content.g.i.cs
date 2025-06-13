9b2c4d22065f511731ab962b1ec06dc86d07a8c2
IsakovIlyaUniversity <isakov.ilya.university@gmail.com>
2025-06-13 19:49:04 +0300

    #4_Main_Menu_2

--- 12Month_WPF_1/12Month_WPF_1/obj/Debug/12Month_WPF_1_Content.g.i.cs
+++ 12Month_WPF_1/12Month_WPF_1/obj/Debug/12Month_WPF_1_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("arrow_back.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("marshak_teremok.jpg")]
 
 
